700fcad29c2a36c1bb85f6f08ab8a9384de03ea7
eugene <sichi331@gmail.com>
2022-12-19 10:46:43 +0200

    Added Dto files

---
+++ TestTaskWebApi/Dto/OriginDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TestTaskWebApi.Dto
+{
+    [Serializable]
+    public class OriginDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("dimension")]
+        public string Dimension { get; set; }
+        
+    }
+}
---
+++ TestTaskWebApi/Dto/PersonDto.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TestTaskWebApi.Dto
+{
+	[Serializable]
+	public class PersonDto
+	{
+
+		//public PersonDto(string json)
+		//{
+		//	JObject jObject = JObject.Parse(json);
+		//	JToken jUser = jObject["results"];
+		//	Name = (string) jUser["name"];
+		//	Status = (string)jUser["status"];
+		//	Species = (string)jUser["species"];
+		//	Type = (string)jUser["type"];
+		//	Gender = (string) jUser["gender"];
+		//}
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+		[JsonProperty("status")]
+		public string Status { get; set; }
+		[JsonProperty("species")]
+		public string Species { get; set; }
+		[JsonProperty("type")]
+		public string Type { get; set; }
+		[JsonProperty("gender")]
+		public string Gender { get; set; }
+		[JsonProperty("origin")]
+		public OriginDto Origin { get; set; }
+
+   //     public PersonDto(string name, string status, string species, string type, string gender)
+   //     {
+			//Name = name;
+			//Status = status;
+			//Species = species;
+			//Type = type;
+			//Gender = gender;
+   //     }
+	}
+}
